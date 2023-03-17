3cd057caa3e313c3a47e7f8076615f7e59303f26
sk naim <sknaimuddin234@gmail.com>
2023-03-17 19:37:31 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,4 @@
 npm i ffmpeg-installer
+ls
 sh ./bin/ffmpeg-installer.sh
 ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls,crypto -i "input.txt" -c:v libx264 -acodec aac -f flv "rtmp://localhost:1935/live/ALongStreamKey"
