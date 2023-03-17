0a43def525898ed8097ad4c6eae2eaef77c2995e
sk naim <sknaimuddin234@gmail.com>
2023-03-17 19:14:57 +0530

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,1 +1,2 @@
+npm install ffmpeg
 ffmpeg -re -f concat -safe 0 -protocol_whitelist file,http,https,tcp,tls,crypto -i "https://cdn.jsdelivr.net/gh/Naim120/Friends@main/inputs%20for%20friends/input.txt?token=GHSAT0AAAAAAB6A2MCC6OQLPWSE4FRVCDKKZASIQQQ" -c:v libx264 -acodec aac -f flv "rtmp://localhost:1935/live/ALongStreamKey"
